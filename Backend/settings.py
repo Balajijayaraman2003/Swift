ae48ce5bcabf456b8757aac1cecc3a52c68f2000
Balajijayarman2003 <jayabalaji2003kdm@gmail.com>
2025-06-29 16:57:51 +0530

    Second Commit

--- Backend/settings.py
+++ Backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-6qn(q@=sh*w6ofi_6e8yz@!dal5l-iwo_0wmy9j4&^rt(+-6@x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
